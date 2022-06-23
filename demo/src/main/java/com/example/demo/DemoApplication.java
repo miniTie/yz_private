8abdbbe432c1022a2c8f4aee33cc7e8d33723a58
Young <1375001394@qq.com>
2022-06-23 15:47:04 +0800

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    ddd
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
